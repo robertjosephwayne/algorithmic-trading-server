ee3fe8485c882d2ba5c2302867ab28accf7dbd7a
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-01-21 23:53:20 +0700

    Add Websocket connection to Alpaca

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
 import dotenv from 'dotenv';
+import Alpaca from '@alpacahq/alpaca-trade-api';
 
 dotenv.config();
 
@@ -15,11 +16,52 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('ping', () => {
-        console.log('ping');
-        socket.emit('pong');
-    });
+    console.log('A new connection has been established.');
+});
+
+class DataStream {
+    alpaca;
+
+    constructor({ apiKey, secretKey, feed, symbol }: any) {
+        this.alpaca = new Alpaca({
+            keyId: apiKey,
+            secretKey,
+            feed,
+        });
+
+        const socket = this.alpaca.crypto_stream_v2;
+
+        socket.onConnect(function () {
+            console.log('Connected');
+            socket.subscribeForBars([symbol]);
+        });
+
+        socket.onError((err) => {
+            console.log(err);
+        });
+
+        socket.onCryptoBar((bar) => {
+            console.log(bar);
+            io.emit('bar', bar);
+        });
+
+        socket.onDisconnect(() => {
+            console.log('Disconnected');
+        });
+
+        socket.connect();
+    }
+}
+
+const feed = 'iex';
+const symbol = 'BTCUSD';
+
+const stream = new DataStream({
+    apiKey: process.env.ALPACA_TRADE_API_KEY,
+    secretKey: process.env.ALPACA_TRADE_SECRET_KEY,
+    feed,
+    symbol,
+    paper: true,
 });
 
 app.get('/', (req: Request, res: Response) => {
