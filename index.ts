29a785e5337b9d89337e4c6e83fce8d4a03063ff
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-01-22 02:47:03 +0700

    Use Polygon API instead of Alpaca API

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
 import dotenv from 'dotenv';
-import Alpaca from '@alpacahq/alpaca-trade-api';
+import { websocketClient } from '@polygon.io/client-js';
 
 dotenv.config();
 
@@ -19,55 +19,17 @@ io.on('connection', (socket) => {
     console.log('A new connection has been established.');
 });
 
-class DataStream {
-    alpaca;
-
-    constructor({ apiKey, secretKey, feed, symbol }: any) {
-        this.alpaca = new Alpaca({
-            keyId: apiKey,
-            secretKey,
-            feed,
-        });
-
-        const socket = this.alpaca.crypto_stream_v2;
-
-        socket.onConnect(function () {
-            console.log('Connected');
-            socket.subscribeForBars([symbol]);
-        });
-
-        socket.onError((err) => {
-            console.log(err);
-        });
-
-        socket.onCryptoBar((bar) => {
-            console.log(bar);
-            io.emit('bar', bar);
-        });
-
-        socket.onDisconnect(() => {
-            console.log('Disconnected');
-        });
-
-        socket.connect();
-    }
-}
-
-const feed = 'iex';
-const symbol = 'BTCUSD';
-
-const stream = new DataStream({
-    apiKey: process.env.ALPACA_TRADE_API_KEY,
-    secretKey: process.env.ALPACA_TRADE_SECRET_KEY,
-    feed,
-    symbol,
-    paper: true,
-});
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
-
 server.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+const POLYGON_API_KEY = process.env.POLYGON_API_KEY || '';
+const cryptoWS = websocketClient(POLYGON_API_KEY).crypto();
+
+cryptoWS.onmessage = ({ data }) => {
+    const [result] = JSON.parse(data);
+    if (result.message === 'authenticated') {
+        cryptoWS.send(JSON.stringify({ action: 'subscribe', params: 'XT.X:BTC-USD' }));
+    }
+    io.emit('bar', result);
+};
