8e834f97a142c7bacbf849ff5627739a8c3cf299
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-01-22 18:25:05 +0700

    Set CORS configuration for REST API

--- index.ts
+++ index.ts
@@ -5,25 +5,30 @@ import express, { Express, Request, Response } from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
 import { websocketClient } from '@polygon.io/client-js';
+import cors from 'cors';
 import apiRouter from './src/routes/api';
 
 const POLYGON_API_KEY = process.env.POLYGON_API_KEY || '';
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 
 const app: Express = express();
 const server = http.createServer(app);
+
+const corsOptions = {
+    origin: CLIENT_URL,
+};
+
 const io = new Server(server, {
-    cors: {
-        origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    },
+    cors: corsOptions,
 });
 
 io.on('connection', (socket) => {
     console.log('A new connection has been established.');
 });
 
-server.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+server.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 
 const cryptoWS = websocketClient(POLYGON_API_KEY).crypto();
@@ -36,4 +41,4 @@ cryptoWS.onmessage = ({ data }) => {
     io.emit('bar', result);
 };
 
-app.use('/api', apiRouter);
+app.use('/api', cors(corsOptions), apiRouter);
