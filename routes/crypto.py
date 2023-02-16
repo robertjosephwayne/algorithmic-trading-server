d0bb29754358a5e46f09e37f8f698c1c0e97e048
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-16 16:15:22 +0700

    Add route: GET /api/crypto/activities

--- routes/crypto.py
+++ routes/crypto.py
@@ -105,7 +105,6 @@ def get_account():
 @crypto_blueprint.route("/positions")
 def get_positions():
     result = rest.list_positions()
-    print(result)
 
     response = []
 
@@ -117,6 +116,28 @@ def get_positions():
             "exchange": asset.exchange,
             "cost_basis": asset.cost_basis,
             "market_value": asset.market_value,
+        })
+
+    return response
+
+
+@crypto_blueprint.route("/activities")
+def get_activities():
+    result = rest.get_activities()
+
+    response = []
+
+    for activity in result:
+        response.append({
+            "activity_type": activity.activity_type,
+            "cumulative_quantity": activity.cum_qty,
+            "order_status": activity.order_status,
+            "price": activity.price,
+            "quantity": activity.qty,
+            "side": activity.side,
+            "symbol": activity.symbol,
+            "transaction_time": activity.transaction_time,
+            "type": activity.type
         })
 
     return response
