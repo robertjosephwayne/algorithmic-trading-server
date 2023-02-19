16948f20217aaff54c1a2a1f6fe43ccc904f824c
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-19 21:09:57 +0700

    Migrate backtesting to VectorBTPro

--- backtest.py
+++ backtest.py
@@ -1,37 +1,34 @@
 import vectorbtpro as vbt
-import numpy as np
-import pandas as pd
 from config import config
 
-vbt.settings.data["custom"]["alpaca"]["client_config"]["api_key"] = config["ALPACA"]["LIVE"]["API_KEY"]
-vbt.settings.data["custom"]["alpaca"]["client_config"]["secret_key"] = config["ALPACA"]["LIVE"]["SECRET_KEY"]
+API_KEY = config["ALPACA"]["LIVE"]["API_KEY"]
+SECRET_KEY = config["ALPACA"]["LIVE"]["SECRET_KEY"]
 
-symbol = 'ETHUSD'
-start = '30 days ago UTC'
-limit = 5000
+symbol = 'BTC/USD'
+start = '2023-02-01 UTC'
+end = '1 hour ago UTC'
+timeframe = "1 hour"
+client_type = "crypto"
 
-# vbt.AlpacaData.set_custom_settings(
-#     client_config=dict(
-#         api_key=config["ALPACA"]["BACKTEST"]["API_KEY"],
-#         secret_key=config["ALPACA"]["BACKTEST"]["SECRET_KEY"],
-#         paper=False
-#     )
-# )
+vbt.AlpacaData.set_custom_settings(
+    client_type=client_type,
+    client_config=dict(
+        api_key=API_KEY,
+        secret_key=SECRET_KEY,
+    )
+)
 
-vbt.AlpacaData.fetch(symbols=symbol, start=start, timeframe="1 hour").get()
-# vbt.AlpacaData.
-# print(data)
-# print(data.get("Close"))
+alpaca_data = vbt.AlpacaData.fetch(symbols=symbol, start=start, end=end, timeframe=timeframe).get("Close")
 
-# alpaca_data = vbt.AlpacaData.download(symbol, start=start, end=end, timeframe=timeframe, exchange="CBSE").get('Close')
-#
-# fast_ma = vbt.MA.run(alpaca_data, 12, short_name='fast')
-# slow_ma = vbt.MA.run(alpaca_data, 24, short_name='slow')
-#
-# entries = fast_ma.ma_crossed_above(slow_ma)
-# exits = fast_ma.ma_crossed_below(slow_ma)
-#
-# pf = vbt.Portfolio.from_signals(alpaca_data, entries, exits)
-#
-# total_return = pf.total_return()
-# print("Total Return: ", total_return)
+fast_ma = vbt.MA.run(alpaca_data, 12, short_name='fast')
+slow_ma = vbt.MA.run(alpaca_data, 24, short_name='slow')
+
+entries = fast_ma.ma_crossed_above(slow_ma)
+exits = fast_ma.ma_crossed_below(slow_ma)
+
+pf = vbt.Portfolio.from_signals(alpaca_data, entries, exits)
+
+total_return = pf.get_total_return()
+print("Total Return: ", total_return)
+
+market_return = pf.get_total_market_return()
