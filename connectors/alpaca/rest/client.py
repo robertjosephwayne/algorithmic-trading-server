b706c166931cb50e4622f78f1e4f8f6dcbb27602
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-19 21:06:08 +0700

    Create file for Alpaca REST client

---
+++ connectors/alpaca/rest/client.py
@@ -0,0 +1,18 @@
+from config import config
+from alpaca_trade_api.rest import URL, REST
+
+base_url = URL("https://paper-api.alpaca.markets")
+api_key = config["ALPACA"]["PAPER"]["API_KEY"]
+secret_key = config["ALPACA"]["PAPER"]["SECRET_KEY"]
+data_feed = "sip"
+
+if config["ALPACA"]["ENABLE_LIVE_TRADING"]:
+    base_url = URL("https://api.alpaca.markets")
+    api_key = config["ALPACA"]["LIVE"]["API_KEY"]
+    secret_key = config["ALPACA"]["LIVE"]["SECRET_KEY"]
+
+alpaca_rest_client = REST(
+    key_id=api_key,
+    secret_key=secret_key,
+    base_url=base_url,
+)
