b69ec6e9486a801d010be1cdab0ee2e3385bafd3
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-19 21:06:45 +0700

    Set up config to toggle between paper and live trading

--- config.py
+++ config.py
@@ -5,14 +5,17 @@ load_dotenv()
 
 config = {
     "ALPACA": {
+        "ENABLE_LIVE_TRADING": True,
         "LIVE": {
             "API_KEY": os.environ.get("ALPACA_LIVE_API_KEY"),
             "SECRET_KEY": os.environ.get("ALPACA_LIVE_API_SECRET"),
         },
-        "BACKTEST": {
-            "API_KEY": os.environ.get("ALPACA_BACKTEST_API_KEY"),
-            "SECRET_KEY": os.environ.get("ALPACA_BACKTEST_API_SECRET"),
+        "PAPER": {
+            "API_KEY": os.environ.get("ALPACA_PAPER_API_KEY"),
+            "SECRET_KEY": os.environ.get("ALPACA_PAPER_API_SECRET"),
         }
     },
     "CLIENT_URL": os.environ.get("CLIENT_URL"),
 }
+
+
