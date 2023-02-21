3815e9ec577ca06cfe82a129b737dcac98455a28
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-21 17:40:23 +0700

    Set max allocation to 1000

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from strategies.moving_average_crossover import MovingAverageCrossoverStrategy
 
 moving_average_crossover = MovingAverageCrossoverStrategy(
-    sma_fast_hours=79, sma_slow_hours=143, max_allocation=5000
+    sma_fast_hours=79, sma_slow_hours=143, max_allocation=1000
 )
 
 
