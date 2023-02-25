b0c322517be820887a4f1b9a4c841f260ca601b0
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-25 23:57:09 +0700

    Extract methods related to account out of strategy. Move to Bot class

--- bot.py
+++ bot.py
@@ -1,9 +1,58 @@
+import math
+from connectors.alpaca.rest.client import alpaca_rest_client
 from strategies.moving_average_crossover import MovingAverageCrossoverStrategy
 
 moving_average_crossover = MovingAverageCrossoverStrategy(
-    sma_fast_hours=79, sma_slow_hours=143, max_allocation=1000
+    sma_fast_hours=79, sma_slow_hours=143
 )
 
 
-async def process_bar(symbol):
-    await moving_average_crossover.process_bar(symbol)
+class Bot:
+    def __init__(self, max_allocation):
+        super().__init__()
+        self._max_allocation = max_allocation
+
+    @staticmethod
+    def _get_position(symbol):
+        positions = alpaca_rest_client.list_positions()
+
+        for p in positions:
+            if p.symbol == symbol:
+                return float(p.qty)
+        return 0
+
+    @staticmethod
+    def _get_non_marginable_buying_power():
+        account = alpaca_rest_client.get_account()
+        return float(account.non_marginable_buying_power)
+
+    def _get_max_position(self):
+        non_marginable_buying_power = self._get_non_marginable_buying_power()
+        return math.floor(min(self._max_allocation, non_marginable_buying_power))
+
+    async def process_bar(self, bar):
+        signal = await moving_average_crossover.process_bar(bar)
+        symbol = bar.symbol.replace("/", "")
+
+        position = self._get_position(symbol=symbol)
+        max_position = self._get_max_position()
+
+        print(f"Current position: {position}")
+        print(f"Max position: {max_position}")
+        print(f"Signal: {signal}")
+
+        if position < max_position and signal:
+            print(
+                f"Symbol: {symbol} / Side: BUY / Notional Amount: {max_position}"
+            )
+            alpaca_rest_client.submit_order(
+                symbol=symbol,
+                notional=max_position,
+                side="buy",
+                time_in_force="gtc",
+            )
+        elif position > 0 and not signal:
+            print(f"Symbol: {symbol} / Side: SELL / Quantity: {position}")
+            alpaca_rest_client.submit_order(
+                symbol=symbol, qty=position, side="sell", time_in_force="gtc"
+            )
--- strategies/moving_average_crossover.py
+++ strategies/moving_average_crossover.py
@@ -5,19 +5,10 @@ from connectors.alpaca.rest.client import alpaca_rest_client
 
 
 class MovingAverageCrossoverStrategy:
-    def __init__(self, sma_fast_hours, sma_slow_hours, max_allocation):
+    def __init__(self, sma_fast_hours, sma_slow_hours):
         super().__init__()
         self._sma_fast_hours = sma_fast_hours
         self._sma_slow_hours = sma_slow_hours
-        self._max_allocation = max_allocation
-
-    @staticmethod
-    def _get_position(symbol):
-        positions = alpaca_rest_client.list_positions()
-        for p in positions:
-            if p.symbol == symbol:
-                return float(p.qty)
-        return 0
 
     @staticmethod
     def _get_sma(series, periods):
@@ -27,15 +18,6 @@ class MovingAverageCrossoverStrategy:
     def _get_signal(fast, slow):
         print(f"Fast {fast[-1]} / Slow: {slow[-1]}")
         return fast[-1] > slow[-1]
-
-    @staticmethod
-    def _get_non_marginable_buying_power():
-        account = alpaca_rest_client.get_account()
-        return float(account.non_marginable_buying_power)
-
-    def _get_max_position(self):
-        non_marginable_buying_power = self._get_non_marginable_buying_power()
-        return math.floor(min(self._max_allocation, non_marginable_buying_power))
 
     def _get_bars(self, symbol):
         print(f"Bar Received: {symbol}")
@@ -61,23 +43,6 @@ class MovingAverageCrossoverStrategy:
         symbol = bar.symbol.replace("/", "")
 
         bars = self._get_bars(symbol=symbol)
-        position = self._get_position(symbol=symbol)
-        should_buy = self._get_signal(bars.sma_fast, bars.sma_slow)
+        signal = self._get_signal(bars.sma_fast, bars.sma_slow)
+        return signal
 
-        max_position = self._get_max_position()
-
-        if position < max_position and should_buy == True:
-            print(
-                f"Symbol: {symbol} / Side: BUY / Notional Amount: {max_position}"
-            )
-            alpaca_rest_client.submit_order(
-                symbol=symbol,
-                notional=max_position,
-                side="buy",
-                time_in_force="gtc",
-            )
-        elif position > 0 and should_buy == False:
-            print(f"Symbol: {symbol} / Side: SELL / Quantity: {position}")
-            alpaca_rest_client.submit_order(
-                symbol=symbol, qty=position, side="sell", time_in_force="gtc"
-            )
