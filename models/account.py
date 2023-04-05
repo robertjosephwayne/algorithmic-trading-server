792a405bf6047b4917d005c9a69c7949562b97d4
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-04-05 15:48:16 +0700

    Update Account get_summary method to use Tradier API

--- models/account.py
+++ models/account.py
@@ -1,16 +1,20 @@
 from connectors.alpaca.rest.client import alpaca_rest_client
+from connectors.tradier.rest.client import tradier_rest_client
 
 
 class Account:
     @staticmethod
     async def get_summary():
-        response = alpaca_rest_client.get_account()
+        balances = tradier_rest_client.get_balances()
 
         summary = {
-            "cash": response.cash,
-            "position_market_value": response.position_market_value,
-            "equity": response.equity,
-            "buying_power": response.buying_power
+            "cash": balances.total_cash,
+            "long_market_value": balances.long_market_value,
+            "short_market_value": balances.short_market_value,
+            "market_value": balances.market_value,
+            "equity": balances.total_equity,
+            "stock_buying_power": balances.margin.stock_buying_power,
+            "option_buying_power": balances.margin.option_buying_power,
         }
 
         return summary
